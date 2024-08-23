0dcfb2fbbe49cffd296f4cf51d1eafe5dedbc7ab
Luciano Mammino <lucianomammino@gmail.com>
2024-08-23 19:08:07 +0100

    chore: small updates

--- 02-the-module-system/15-import-esm-from-cjs/main.cjs
+++ 02-the-module-system/15-import-esm-from-cjs/main.cjs
@@ -1,6 +1,7 @@
 'use strict'
 
 // This will throw an ERR_REQUIRE_ESM error
+// unless you use the node flag `--experimental-require-module`
 const { someFeature } = require('./someModule.mjs')
 
 console.log(someFeature)
