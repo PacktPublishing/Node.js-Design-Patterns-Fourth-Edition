6a02de4a2c2193248e9f1372e22f47689fbba463
Luciano Mammino <lucianomammino@gmail.com>
2024-09-15 12:26:39 +0100

    chore: working on chapter 3

--- 03-callbacks-and-events/15-events-plus-callbacks/index.js
+++ 03-callbacks-and-events/15-events-plus-callbacks/index.js
@@ -3,21 +3,27 @@ import { get } from 'node:https'
 
 function download(url, cb) {
   const eventEmitter = new EventEmitter()
-  get(url, resp => {
-    const len = Number.parseInt(resp.headers['content-length'], 10)
+
+  const req = get(url, resp => {
     const chunks = []
-    let downloaded = 0
-    resp.on('data', chunk => {
-      chunks.push(chunk)
-      downloaded += chunk.length
-      eventEmitter.emit('progress', downloaded, len)
-    })
-    resp.on('end', () => {
-      const data = Buffer.concat(chunks)
-      eventEmitter.emit('complete', data)
-      cb(null, data)
-    })
-  }).on('error', err => {
+    let downloadedBytes = 0
+    const fileSize = Number.parseInt(resp.headers['content-length'], 10)
+    resp
+      .on('error', err => {
+        cb(err)
+      })
+      .on('data', chunk => {
+        chunks.push(chunk)
+        downloadedBytes += chunk.length
+        eventEmitter.emit('progress', downloadedBytes, fileSize)
+      })
+      .on('end', () => {
+        const data = Buffer.concat(chunks)
+        cb(null, data)
+      })
+  })
+
+  req.on('error', err => {
     cb(err)
   })
 
@@ -28,9 +34,9 @@ download(
   'https://www.nodejsdesignpatterns.com/img/node-js-design-patterns.jpg',
   (err, data) => {
     if (err) {
-      return console.error(`Error downloading data: ${err.message}`)
+      return console.error(`Download failed: ${err.message}`)
     }
-    console.log('Downloaded', data)
+    console.log('Download completed', data)
   }
 ).on('progress', (downloaded, total) => {
   console.log(
